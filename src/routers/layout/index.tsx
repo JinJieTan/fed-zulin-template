6ced5319f80e2f7f8042a73e12f585a8af34f5c1
谭金杰 <tanjj02@mingyuanyun.com>
2020-07-09 12:09:41 +0800

    feat: remove

--- src/routers/layout/index.tsx
+++ src/routers/layout/index.tsx
@@ -106,9 +106,7 @@ class Layout extends React.Component<Props, State> {
                     <Content style={{ overflowX: 'auto' }}>
                         <div style={{ minWidth: '1208px', height: '100%' }}>{children}</div>
                     </Content>
-                    <Footer className="main-footer">
-                        Copyright © {new Date().getFullYear()} 明源云空间 版权所有 鄂ICP备15101856号-1
-                    </Footer>
+                    <Footer className="main-footer">Copyright © {new Date().getFullYear()}</Footer>
                 </AntLayout>
             </AntLayout>
         );
