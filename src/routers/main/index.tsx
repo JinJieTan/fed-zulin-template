e4847b257b227123904829ae7a39f500b85eba7d
tanjj02@mingyuanyun.com <tanjj02@mingyuanyun.com>
2021-06-17 10:23:19 +0800

    fix: fix bug

--- src/routers/main/index.tsx
+++ src/routers/main/index.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { registerMicroApps, start } from 'qiankun';
+
 import './index.less';
 export default function App() {
-    return <div className='micro-content'></div>;
+    useEffect(() => {
+
+        // registerMicroApps([
+        //     {
+        //         name: 'react app',
+        //         entry: '//localhost:7100',
+        //         container: '#micro-content',
+        //         activeRule: '/react16',
+        //     },
+        //     {
+        //         name: 'vue app',
+        //         entry: { scripts: ['//localhost:7100/main.js'] },
+        //         container: '#micro-content',
+        //         activeRule: '/react15',
+        //     },
+        // ]);
+
+        // start();
+    }, []);
+    return <div id='micro-content'></div>;
 }
